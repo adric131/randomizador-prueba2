c6672e858cf2ad97bbd5fb02bb14c9415aa6da74
adric131 <adrianroldan131@gmail.com>
2023-05-18 22:23:35 -0300

    cambios

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const textoRandomizar = document.getElementById("randomizar-texto")
 
 divBoton.addEventListener("click", (e) => {
 
-const rutaDeImagenes = ["../fotos/Champs/Gragasicono.webp", "../fotos/Champs/samira.png","../fotos/Champs/kassadin.png","../fotos/Champs/Gnar.webp","../fotos/Champs/lulu.webp","../fotos/Champs/zoe.png","../fotos/Champs/Rakan.webp","../fotos/Champs/doctormundo.webp","../fotos/Champs/Morgana.webp","../fotos/Champs/darius.png","../fotos/Champs/nami.webp","../fotos/Champs/Morde.webp","../fotos/Champs/ww.webp",
-"../fotos/Champs/Cait.webp","../fotos/Champs/lux.webp","../fotos/Champs/Ornn.webp","../fotos/Champs/singed.webp","../fotos/Champs/jinx.webp","../fotos/Champs/sett.webp","../fotos/Champs/Garen.webp","../fotos/Champs/vi.webp","../fotos/Champs/Lucian.webp","../fotos/Champs/Senna.webp","../fotos/Champs/Syndra.webp","../fotos/Champs/Xin.webp","../fotos/Champs/varus.webp","../fotos/Champs/Heimer.webp","../fotos/Champs/Yuumi.webp",
-"../fotos/Champs/Blit.webp","../fotos/Champs/jhin.webp","../fotos/Champs/xayah.webp","../fotos/Champs/K6.webp","../fotos/Champs/Rengar.webp","../fotos/Champs/Elise.webp","../fotos/Champs/Nida.webp","../fotos/Champs/Ksante.webp","../fotos/Champs/fiora.webp","../fotos/Champs/Nasus.webp","../fotos/Champs/lee.webp","../fotos/Champs/yorick.webp"];
+const rutaDeImagenes = ["/fotos/Champs/Gragasicono.webp", "/fotos/Champs/samira.png","/fotos/Champs/kassadin.png","/fotos/Champs/Gnar.webp","/fotos/Champs/lulu.webp","/fotos/Champs/zoe.png","/fotos/Champs/Rakan.webp","/fotos/Champs/doctormundo.webp","/fotos/Champs/Morgana.webp","/fotos/Champs/darius.png","/fotos/Champs/nami.webp","/fotos/Champs/Morde.webp","/fotos/Champs/ww.webp",
+"/fotos/Champs/Cait.webp","/fotos/Champs/lux.webp","/fotos/Champs/Ornn.webp","/fotos/Champs/singed.webp","/fotos/Champs/jinx.webp","/fotos/Champs/sett.webp","/fotos/Champs/Garen.webp","/fotos/Champs/vi.webp","/fotos/Champs/Lucian.webp","/fotos/Champs/Senna.webp","/fotos/Champs/Syndra.webp","/fotos/Champs/Xin.webp","/fotos/Champs/varus.webp","/fotos/Champs/Heimer.webp","/fotos/Champs/Yuumi.webp",
+"/fotos/Champs/Blit.webp","/fotos/Champs/jhin.webp","/fotos/Champs/xayah.webp","/fotos/Champs/K6.webp","../fotos/Champs/Rengar.webp","../fotos/Champs/Elise.webp","../fotos/Champs/Nida.webp","../fotos/Champs/Ksante.webp","../fotos/Champs/fiora.webp","../fotos/Champs/Nasus.webp","../fotos/Champs/lee.webp","../fotos/Champs/yorick.webp"];
  
 
 const Randomizar = Math.floor((Math.random()*40));
