eb493781257749a2ea7c705aeb0c22fb9d3d49e4
adric131 <adrianroldan131@gmail.com>
2023-06-08 23:17:02 -0300

    cambios a las imagens

--- main.js
+++ main.js
@@ -84,8 +84,8 @@ divBoton.addEventListener("click", () => {
 
 divBoton.addEventListener("click", () => {
 
-    const rutaDeSummoners = ["../Tp.webp", "../ex.webp", "../barrera.webp", "../ignite.webp", "../purificar.webp"];
-    const rutaDeSummonersInamovible = ["../Flash.webp", "../fantsamal.webp", "../curar.webp", "../Smite.webp"];
+    const rutaDeSummoners = ["/fotos/Summoners/Tp.webp", "/fotos/Summoners/ex.webp", "/fotos/Summoners/barrera.webp", "/fotos/Summoners/ignite.webp", "/fotos/Summoners/purificar.webp"];
+    const rutaDeSummonersInamovible = ["/fotos/Summoners/Flash.webp", "/fotos/Summoners/fantsamal.webp", "/fotos/Summoners/curar.webp", "/fotos/Summoners/Smite.webp"];
     const randomizarSummoners1 = Math.floor((Math.random()) * 4);
     const randomizarSummoners2 = Math.floor((Math.random()) * 5)
 
